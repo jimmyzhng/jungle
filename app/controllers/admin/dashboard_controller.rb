0f3dc87fcac0db3a50ad054492d1e83e7146e69e
Jimmy <jimmyzhang1@hotmail.com>
2023-01-28 01:19:27 -0800

    Create admin dashboard page that displays product count and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["AUTH_USERNAME"], password: ENV["AUTH_PASSWORD"]
-
+  http_basic_authenticate_with name: 'jungle', password: 'book'
 
   def show
+   @products = Product.count
+   @categories = Category.count
   end
+  
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV["AUTH_USERNAME"], password: ENV["AUTH_PASSWORD"]
+  http_basic_authenticate_with name: 'jungle', password: 'book'
   
   def index
     @products = Product.order(id: :desc).all

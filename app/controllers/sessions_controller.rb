4abc5026cf416a501c3a504d2f1a002d677e4074
Jimmy <jimmyzhang1@hotmail.com>
2023-01-28 23:28:34 -0800

    Add login/logout authentication with bcrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    # If user exists, and pw entered is correct
+    if user && user.authenticate(params[:password])
+      # save user id inside the browser cookie. this keeps user logged in
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # if incorrect, we go back to the form
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+  end

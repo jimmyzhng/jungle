47def9d8c3611adda033408978ced940c20e9645
Jimmy <jimmyzhang1@hotmail.com>
2023-01-30 18:59:59 -0800

    Add rspec

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: 'jungle', password: 'book'
+  http_basic_authenticate_with name: ENV["AUTH_USERNAME"], password: ENV["AUTH_PASSWORD"]
 
   def show
    @products = Product.count
